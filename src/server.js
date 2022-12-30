5c8d5954cc49b32987127fce3174942de8bb3b4c
Farhan Hilmi <farhanhilmi077@gmail.com>
2022-12-30 20:17:22 +0700

    change server

--- src/server.js
+++ src/server.js
@@ -3,13 +3,17 @@ import config from './config/index.js';
 
 import app from './app.js';
 
-try {
-    await mongoose
-        .connect(config.db.uri)
-        .then(() => console.log('Connected to DB'));
-    app.listen(config.app.port, () => {
-        console.log(`Server running on port ${config.app.port}`);
-    });
-} catch (err) {
-    console.log(err);
-}
+const startServer = async () => {
+    try {
+        await mongoose
+            .connect(config.db.uri)
+            .then(() => console.log('Connected to DB'));
+        app.listen(config.app.port, () => {
+            console.log(`Server running on port ${config.app.port}`);
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+startServer();
